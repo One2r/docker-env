484a3dcd8d8954efdba768451858539a56201c28
One2r <1972871083@qq.com>
2018-03-08 22:55:33 +0800

    m

--- lnmp/etc/run.sh
+++ lnmp/etc/run.sh
@@ -2,4 +2,4 @@
 /usr/local/openresty/bin/openresty
 /usr/local/php/sbin/php-fpm
 /usr/local/redis/bin/redis-server /usr/local/redis-3.2.11/etc/redis.conf
-/etc/init.d/mysqld start
+#/etc/init.d/mysqld start
