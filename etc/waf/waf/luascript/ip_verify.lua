9b58ebe582d9c5a36ab582e9ec37e933cd5a32b3
吴辉 <1972871083@qq.com>
2023-06-13 13:54:57 +0800

    perform lua

--- etc/waf/waf/luascript/ip_verify.lua
+++ etc/waf/waf/luascript/ip_verify.lua
@@ -10,12 +10,14 @@ red:set_timeouts(1000, 1000, 1000) -- 1 sec
 local ok, err = red:connect(config:get("redis-host"), config:get("redis-port"))
 if not ok then
     ngx.log(ngx.ERR, "Failed to connect redis: ",err)
+    red:close()
     ngx.exec("@proxy-by-waf");
 end
 
 local ok, err = red:auth(config:get("redis-password"))
 if not ok then
     ngx.log(ngx.ERR, "Failed to authenticate redis: ",err)
+    red:close()
     ngx.exec("@proxy-by-waf");
 end
 
@@ -25,16 +27,20 @@ local ip = ngx.var.remote_addr
 local res, err = red:cf():exists("waf:ip:blacklist",ip)
 if not res then
     ngx.log(ngx.ERR, "Failed to execute cf.exists for blacklist: ",err)
+    red:set_keepalive(10000, 100)
     ngx.exec("@proxy-by-waf");
 end
 
 if res == 1 then
     ngx.log(ngx.ERR, "Access denied with code 403. Matched remote_addr in blacklist")
+    red:set_keepalive(10000, 100)
     ngx.exit(ngx.HTTP_FORBIDDEN)
 end
 
 -- whitelist
 local res, err = red:cf():exists("waf:ip:whitelist",ip)
+red:set_keepalive(10000, 100)
+
 if not res then
     ngx.log(ngx.ERR, "Failed to execute cf.exists for whitelist: ",err)
     ngx.exec("@proxy-by-waf");
@@ -45,5 +51,3 @@ if res == 0 then
 else
     ngx.exec("@proxy");
 end
-
-local ok, err = red:set_keepalive(10000, 512)
