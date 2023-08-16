1d46b31256485d3291df7057a1b70be107704f8d
此H非彼h <1972871083@qq.com>
2023-08-16 14:55:36 +0800

    Delete init.lua

--- etc/gateway/init.lua
+++
@@ -1,7 +0,0 @@
-local cb_factory = require "circuitbreaker"
-
-local config = ngx.shared.waf_config
-
-config:set("redis-host", "redis")
-config:set("redis-port", 6379)
-config:set("redis-password", "Admin123?")
