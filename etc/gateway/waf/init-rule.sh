3834d31c5837f567a626b3099d81f8717255d8d9
吴辉 <1972871083@qq.com>
2023-08-17 15:49:58 +0800

    upgrade crs to 3.3.5

--- etc/gateway/waf/init-rule.sh
+++ etc/gateway/waf/init-rule.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 mkdir modsec custom
 
-wget https://raw.githubusercontent.com/SpiderLabs/ModSecurity/v3/master/modsecurity.conf-recommended -o modsec/modsecurity.conf
+wget -O modsec/modsecurity.conf https://raw.githubusercontent.com/SpiderLabs/ModSecurity/v3/master/modsecurity.conf-recommended
 
-wget https://raw.githubusercontent.com/SpiderLabs/ModSecurity/v3/master/unicode.mapping -o modsec/unicode.mapping
+wget -O modsec/unicode.mapping https://raw.githubusercontent.com/SpiderLabs/ModSecurity/v3/master/unicode.mapping 
 
-wget https://github.com/coreruleset/coreruleset/archive/refs/tags/v3.3.4.tar.gz -o crs3.tar.gz && tar -xzvf crs3.tar.gz && rm -rf crs3.tar.gz
-cp coreruleset-3.3.4/crs-setup.conf.example coreruleset-3.3.4/crs-setup.conf
+wget -O crs3.tar.gz https://github.com/coreruleset/coreruleset/archive/refs/tags/v3.3.5.tar.gz  && tar -xzvf crs3.tar.gz && rm -rf crs3.tar.gz
+cp coreruleset-3.3.5/crs-setup.conf.example coreruleset-3.3.5/crs-setup.conf
